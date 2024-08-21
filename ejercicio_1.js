8f13a527c6b96dc743ee2a1fc0b05e578d75c23d
emmanuel cardona <emmauel1033489424@gmail.com>
2024-08-21 12:10:00 -0500

    ejercicio1

---
+++ ejercicio_1.js
@@ -0,0 +1,11 @@
+//  (EJERCICIO 1)Comparar edad
+
+let edad = prompt("Ingrese su edad");
+
+if (edad >= 18) {
+  document.write("Eres mayor de edad puedes, ingresar");
+} else if (edad < 18) {
+  document.write("Eres menor de edad, no puedes ingresar");
+}
+document.write ('<br>')
+
